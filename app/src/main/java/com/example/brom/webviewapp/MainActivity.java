d8d59cb3f6c8a1d82e79070d8e964a7efae0f320
a16silph <a16silph@student.his.se>
2019-04-12 15:31:29 +0200

    Input of my own app-prototype, Average Joe.

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
     // Create a private member variable that can hold our WebView
@@ -20,15 +21,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // The FAB-code can be removed
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Lägger till en WebView element.
+        WebView webView = new WebView(this);
+
+        // Skapar så att elementen blir aktiv.
+        setContentView(webView);
+
+        // Implentering av egen app-prototyp.
+        webView.loadUrl("https://wwwlab.iit.his.se/a16silph/mobilapplikation/Project%20Dalucci/Index.html");
+
+
         // 1. Create a WebView element in the layout file content_main.xml
         // -- Commit and push to your github fork
         // 2. Give the WebView element created in step 1 ID "my_webview"
